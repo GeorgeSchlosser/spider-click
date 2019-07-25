7f6e7a55570c6123a3ca590f259c7ca7492638dc
Trash Panda <d.george.schlosser@gmail.com>
2019-07-25 15:15:34 -0400

    Updates

---
+++ precache-manifest.825d512124293bb0aa3866dc6af6e3b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b19f5b1c91f107a06d5be66def96796",
+    "url": "/spider-click/index.html"
+  },
+  {
+    "revision": "c1b49d609ddb663e8329",
+    "url": "/spider-click/static/css/main.fbc595d7.chunk.css"
+  },
+  {
+    "revision": "874b9de250a5cb96c340",
+    "url": "/spider-click/static/js/2.55060c2c.chunk.js"
+  },
+  {
+    "revision": "c1b49d609ddb663e8329",
+    "url": "/spider-click/static/js/main.3da0c4e9.chunk.js"
+  },
+  {
+    "revision": "66a26f557460706bcace",
+    "url": "/spider-click/static/js/runtime~main.14511575.js"
+  }
+]);
